f391ef8b14b32716ec63e6a69400d954c02daae2
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-04-17 18:58:14 -0400

    saving work

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, select: false },
+    messages : [{ type: Schema.Types.ObjectId, ref: "Message" }]
+})
+  
+const User = mongoose.model('User', UserSchema)
+  
+module.exports = User
